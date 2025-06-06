b89100c3bd3e7e9683e19291e7d8ac0f65a1e313
MooniPuss <140527383+ImmanuelClaerhout@users.noreply.github.com>
2025-06-06 06:04:17 +0200

    Finalization

---
+++ Assets/AssetsFinalScene/Scripts/ScreenShotTaker.cs
@@ -0,0 +1,35 @@
+//by Dante Deketele
+//DAE 2023-2024
+
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ScreenShotTaker : MonoBehaviour
+{
+    [SerializeField] private KeyCode shootButton;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(shootButton))
+        {
+            TakeScreenshot();
+        }
+    }
+
+    static void TakeScreenshot(int index = 0)
+    {
+        if (Application.isPlaying)
+        {
+            //Make sure you have the Screenshots folder set up, the script won't generate it/
+            string path = "Assets/Screenshots";
+            var time = System.DateTime.Now;
+            string timestamp = $"{time.Hour}{time.Minute}{time.Second}";
+            string fileName = $"{path}/screenshot_{index}-{timestamp}.png";
+
+            ScreenCapture.CaptureScreenshot(fileName);
+
+            Debug.Log("Took screenshot: " + fileName);
+        }
+    }
+}
